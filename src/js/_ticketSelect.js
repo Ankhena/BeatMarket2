eb02b38fa4f3987ba4aff8dbb9e03c89b5b60ec3
Matvey <matvejaksentev80@gmail.com>
2022-09-03 12:50:03 +0300

    Last change

--- src/js/_ticketSelect.js
+++ src/js/_ticketSelect.js
@@ -1,7 +1,16 @@
 function initTicketSelect() {
-    $('.inputSelect__radio').on('change', function(){
-        $('.inputSelect__value').html($(this).next().html());
-    });
+  document.querySelectorAll(".inputSelect").forEach((item, i) => {
+    const radioes = item.querySelectorAll(".inputSelect__item");
+    const value = item.querySelector(".inputSelect__value")
+    radioes.forEach(radio => {
+      const listText = radio.querySelectorAll(".inputSelect__choose")
+      radio.addEventListener('change', function () {
+        listText.forEach(text => {
+          value.textContent = text.textContent
+        })
+      });
+    })
+  })
 }
 
 initTicketSelect();
